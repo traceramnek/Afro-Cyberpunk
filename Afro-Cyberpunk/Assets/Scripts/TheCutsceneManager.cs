9e0ca25b01ab0a2e30ac13f99e5c445a52860798
Gman9000 <granvillejn@aol.com>
2019-06-12 15:06:30 -0400

    CutScene POF
    Unity Timeline cutscene was created and scripts created to handle the demo cutscene (cutscene lasts about 10 seconds). Check Gman scene for the changes

---
+++ Afro-Cyberpunk/Assets/Scripts/TheCutsceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TheCutsceneManager : MonoBehaviour
+{
+    public GameObject thePlayer;
+    private Vector3 playerEndingPosition;
+    private bool fix;
+    public Animator playerAnimator;
+    private RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        fix = false;
+        //this cutscene requires that the player is facing the right
+        thePlayer.transform.localScale = new Vector3(Mathf.Abs(thePlayer.transform.localScale.x), thePlayer.transform.localScale.y, thePlayer.transform.localScale.z);
+        thePlayer.GetComponent<PlayerPlatformer>().enabled = false;
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((director.state != PlayState.Playing) && (!fix))
+        {
+            fix = true;
+            playerEndingPosition = thePlayer.transform.position;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+            thePlayer.transform.position = playerEndingPosition;
+            playerAnimator.SetBool("grounded", true);
+            thePlayer.GetComponent<PlayerPlatformer>().enabled = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
