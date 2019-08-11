9245b29278bbff9adf9f72898d70c59cf90972ea
Kwan Holloway <kwansholloway@gmail.com>
2019-08-10 21:09:20 -0400

    enemy sight update

--- Afro-Cyberpunk/Assets/Scripts/EnemySight.cs
+++ Afro-Cyberpunk/Assets/Scripts/EnemySight.cs
@@ -10,25 +10,24 @@ public class EnemySight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Physics2D.queriesStartInColliders = false; // ray does not detect it's own objects collider
+        Physics2D.queriesStartInColliders = false; // so ray does not detect it's own objects collider
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Vector3 target = new Vector3(transform.position.x + 90, transform.position.y, 0);
-        // Debug.DrawLine(transform.position, target, Color.red);
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
-        Debug.DrawLine(transform.position, hitInfo.point, Color.red);
-        // if (hitInfo.collider != null && hitInfo.collider.CompareTag("Player"))
-        // {
-        //     Debug.DrawRay(transform.position, hitInfo.point, Color.red);
-        // }
-        // else
-        // {
-        //     Debug.DrawRay(transform.position, transform.position + transform.right * distance, Color.green);
-        // }
+    
+        if (hitInfo.collider != null && hitInfo.collider.CompareTag("Player"))
+        {
+            Debug.Log(hitInfo.collider);
+            Debug.DrawRay(transform.position, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.right * distance, Color.green);
+        }
     }
 
     void RaycastLogic()
